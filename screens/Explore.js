baef3eb43587ad1648161edffaae884eb86cfe37
norizon-tg <dizonnorman543@gmail.com>
2020-07-09 04:06:29 +0800

    Explore: Fetching data from Firebase

--- screens/Explore.js
+++ screens/Explore.js
@@ -22,29 +22,9 @@ import database from '@react-native-firebase/database';
 
 
 const { width, height } = Dimensions.get("window");
-const items = [
-  { name: 'TURQUOISE', code: '#1abc9c' },
-  { name: 'EMERALD', code: '#2ecc71' },
-  { name: 'PETER RIVER', code: '#3498db' },
-  { name: 'AMETHYST', code: '#9b59b6' },
-  { name: 'WET ASPHALT', code: '#34495e' },
-  { name: 'GREEN SEA', code: '#16a085' },
-  { name: 'NEPHRITIS', code: '#27ae60' },
-  { name: 'BELIZE HOLE', code: '#2980b9' },
-  { name: 'WISTERIA', code: '#8e44ad' },
-  { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-  { name: 'SUN FLOWER', code: '#f1c40f' },
-  { name: 'CARROT', code: '#e67e22' },
-  { name: 'ALIZARIN', code: '#e74c3c' },
-  { name: 'CLOUDS', code: '#ecf0f1' },
-  { name: 'CONCRETE', code: '#95a5a6' },
-  { name: 'ORANGE', code: '#f39c12' },
-  { name: 'PUMPKIN', code: '#d35400' },
-  { name: 'POMEGRANATE', code: '#c0392b' },
-  { name: 'SILVER', code: '#bdc3c7' },
-  { name: 'ASBESTOS', code: '#7f8c8d' },
-];
 
+
+   
 
 class Explore extends Component {
   state = {
@@ -92,15 +72,19 @@ class Explore extends Component {
 
   
   renderExplore() {
-    const userId = (firebase.auth().currentUser.uid).toString();
+
+    const items = [];
+    const userId = (firebase.auth().currentUser.uid);
     var rootRef = firebase.database().ref("Items");
     var urlRef = rootRef.child(userId);
-    urlRef.once("value", function(snapshot) {
-      snapshot.forEach(function(child) {
-        console.log(child.key+": "+child.val());
+    urlRef.on('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var childData = childSnapshot.val().itemName;
+        console.log(childData);
+        var itemName = childData.toString();
+        items.push(itemName);
       });
-    });
-
+  });
 
     return (
       <FlatGrid
@@ -111,7 +95,7 @@ class Explore extends Component {
       // fixed
       spacing={10}
       renderItem={({ item }) => (
-        <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
+        <View style={[styles.itemContainer, { backgroundColor: "#00FF00" }]}>
           <Text style={styles.itemName}>{item.name}</Text>
           {/* <Text style={styles.itemCode}>{item.code}</Text> */}
         </View>
